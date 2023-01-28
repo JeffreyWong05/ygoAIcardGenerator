660c2901ab383f26210e918cef67b68bc82631a9
NathanielCode <nathanielcode@gmail.com>
2023-01-28 01:31:41 -0500

    Name and attribute

--- React/ygocardgenerator/src/App.js
+++ React/ygocardgenerator/src/App.js
@@ -2,6 +2,17 @@ import './App.css';
 import yugi from './images/yamiyugi.png';
 
 function App() {
+
+  const attributeDict = {
+    "dark": require('./images/Dark.webp'),
+    "divine": require('./images/Divine.webp'),
+    "earth": require('./images/Earth.webp'),
+    "fire": require('./images/Fire.webp'),
+    "light": require('./images/Light.webp'),
+    "water": require('./images/Water.png'),
+    "wind": require('./images/Wind.webp'),
+  }
+
   return (
     <div className="page">
       <h1 className="title">Yugioh Card Generator</h1>
@@ -17,11 +28,9 @@ function App() {
           <div className="yugName">
             Zexi the Invoker
           </div>
-
+          <img src={attributeDict["earth"]} className="attribute"></img>
           </div>
-
         </div>
-        
       </div>
       <img className="yugiImg" 
         src={yugi} 
