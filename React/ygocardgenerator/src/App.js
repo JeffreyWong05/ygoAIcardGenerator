2d51278b916d4ab6efed7b1ae3c0d7d73df225bc
zexi <zexi.lv@mail.utoronto.ca>
2023-01-28 00:19:12 -0500

    began ui

--- React/ygocardgenerator/src/App.js
+++ React/ygocardgenerator/src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="page">
+      <h1 className="title">Yugioh Card Generator</h1>
+      <div className="entry">
+        <label>Enter Card Name:</label>
+        <input className="monsterName" type="text"/>
+        <input type="submit" value="Submit" />
+      </div>
+
       <div className="yugiohTemplate">
         <div className="nameAndAttribute">
         <div className="yugName">
