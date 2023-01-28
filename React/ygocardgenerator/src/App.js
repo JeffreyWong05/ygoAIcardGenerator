1b4342f91a3dedf6e062f079119ec46d21b06850
Brandon Ye <57378354+yebrandon@users.noreply.github.com>
2023-01-28 02:17:08 -0500

    merge details generator with app.js

--- React/ygocardgenerator/src/App.js
+++ React/ygocardgenerator/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import yugi from './images/yamiyugi.png';
 import React, { useState } from 'react';
+import { Configuration, OpenAIApi } from 'openai';
 
 function App() {
 
   const [cardName, setCardName] = useState('');
-  const [cardAttribute, setCardAttribute] = useState("")
+  const [cardAttribute, setCardAttribute] = useState("");
+  const [results, setResults] = useState({
+		effect: '',
+		image: '',
+		level: ''
+	});
+	const [loading, setLoading] = useState(false);
 
   const attributeDict = {
     "dark": require('./images/Dark.webp'),
@@ -17,6 +24,65 @@ function App() {
     "wind": require('./images/Wind.webp'),
     "": null,
   }
+  const prompts = {
+		effect: `Write a YuGiOh effect for a card named "${cardName}"
+        Effect:`,
+		image: cardName,
+		level: `Rate the YuGiOh card\'s strength from 1 to 12 
+        YuGiOh card:"${cardName}"
+        Strength:`
+	};
+	const configuration = new Configuration({
+		apiKey: `${process.env.REACT_APP_API_KEY}`
+	});
+
+  const openai = new OpenAIApi(configuration);
+
+  const generateDetails = async () =>
+  {
+    console.log("generate");
+		setLoading(true);
+		let response;
+
+		for (let key in prompts) {
+			if (key == 'image') {
+				response = await openai.createImage({
+					prompt: prompts.image,
+					n: 1,
+					size: '512x512'
+				});
+				setResults((prevResults) => ({
+					...prevResults,
+					image: response.data.data[0].url
+				}));
+			} else {
+				response = await openai.createCompletion({
+					model: 'text-davinci-003',
+					prompt: prompts[key],
+					temperature: 0.6,
+					max_tokens: 150,
+					top_p: 1,
+					frequency_penalty: 1,
+					presence_penalty: 1
+				});
+				setResults((prevResults) => ({
+					...prevResults,
+					[key]: response.data.choices[0].text
+				}));
+			}
+		}
+		setLoading(false);
+	};
+
+	const displayResults = () => {
+		return Object.keys(results).map((key) => (
+			<div>
+				{results[key]}
+				<br />
+				<br />
+			</div>
+		));
+	};
 
   return (
     <div className="page">
@@ -24,18 +90,23 @@ function App() {
       <div className="entry">
         <label>Enter Card Name:</label>
         <input className="monsterName" type="text" onChange={(e) => setCardName(e.target.value)}/>
-        <input type="submit" value="Submit"/>
+        <button className='button' onClick={generateDetails}>
+					Generate Card
+				</button>
       </div>
 
       <div className="lcol">
         <div className="yugiohTemplate">
           <div className="nameAndAttribute">
-          <div className="yugName">
-            {cardName}
+            <div className="yugName">
+              {cardName}
+            </div>
+            {cardAttribute === "" 
+            ? null 
+            : <img src={attributeDict[cardAttribute]} className="attribute"></img>}
           </div>
-          {cardAttribute === "" 
-          ? null 
-          : <img src={attributeDict[cardAttribute]} className="attribute"></img>}
+          <div>
+            {displayResults()}
           </div>
         </div>
       </div>
